3e0dccdf49d600bbd450cfc7b7af10c27aef4ab3
Christian Binay-an <christianbinayan3@gmail.com>
2022-12-06 19:38:59 +0800

    Updates

---
+++ precache-manifest.1dc2150ecd8dd93a89f3edd902e93120.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99730467eeec23ecf3bc",
+    "url": "/course-directory/static/css/main.440b609c.chunk.css"
+  },
+  {
+    "revision": "99730467eeec23ecf3bc",
+    "url": "/course-directory/static/js/main.99730467.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "75824219963eb37769ad8e7094b186ad",
+    "url": "/course-directory/index.html"
+  }
+];
